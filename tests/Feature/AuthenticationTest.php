cf9cfa52f871f2de0545347384f2f99ba869bb62
Laszlo Csoka <laszlo@eclick.co.hu>
2021-02-14 23:42:53 +0100

    (feat) authentication tests started

---
+++ tests/Feature/AuthenticationTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthenticationTest extends TestCase
+{
+
+
+    public function testRequiredFieldsForRegistration()
+    {
+        $this->json('POST', 'api/auth/register', ['Accept' => 'application/json'])
+            ->assertStatus(422)
+            ->assertJson([
+                "message" => "The given data was invalid.",
+                "errors" => [
+                    "name" => ["The name field is required."],
+                    "email" => ["The email field is required."],
+                    "password" => ["The password field is required."],
+                ]
+            ]);
+    }
+}
